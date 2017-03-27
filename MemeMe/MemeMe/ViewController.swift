b3f761f2573424e56696084e6d33004132e049db
aniketghode <aniketghode@Anikets-MacBook-Air.local>
2017-03-27 21:16:50 +0530

    Added IBOutlets and IBActions Added ImagePickerControllerDelegate: Selected image displayed in ImageVIew Added TextAttributes to TextField (In Progress).

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    //MARK: Buttons Tag
+    enum TabBarButtons: Int {
+        case Camera = 0, Album
+    }
+
+    
+    //MARK:- Properties
+    
+    @IBOutlet weak var bottomTextField: UITextField!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imagePickerView: UIImageView!
+    var imagePicker: UIImagePickerController!
+    
+    // TextField TextAttributes
+    let memeTextAttributes: [String: Any] = [
+        NSStrokeColorAttributeName: UIColor.black,
+        NSForegroundColorAttributeName: UIColor.white,
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: 2.5
+    ]
+    
+    //MARK:- View Controller lifecycle methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        topTextField.defaultTextAttributes = memeTextAttributes
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // check if camera is available and enable/disable the camera button
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK:- IBActions
+    @IBAction func pickAnImage(_ sender: UIBarButtonItem) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        switch (TabBarButtons(rawValue: sender.tag)!) {
+        case .Camera:
+            imagePicker.sourceType = .camera
+        case .Album:
+            imagePicker.sourceType = .photoLibrary
+        }
+        present(imagePicker, animated: true, completion: nil)
     }
-
+    
+    //MARK:- UIImagePickerControllerDelegate Methods
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Pick image and display it in imageView
+        print(info)
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage {
+            imagePickerView.image = image
+        }
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        // dismiss the image picker dialog
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
 
 }
 
